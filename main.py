703f2e69036ae95e4f33b5fba8795fd04ae76526
CommanderKV <71740821+CommanderKV@users.noreply.github.com>
2022-06-21 13:33:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
+"""
+This program will:
+    "Add student" button pressed:
+        - add a student to a save file with all of the details required
+
+    "Schedule event" button pressed:
+        - Get students who have not done many events
+        - Get the appropriate ammount of students for the event
+        - Show user
+
+"""
+
+
+
+
+import tkinter
 
 def main():
-    pass
+    window = tkinter.Tk()
+
+    WIN_WIDTH = window.winfo_screenwidth()
+    WIN_HEIGHT = window.winfo_screenheight()
+    window.geometry(f"{WIN_WIDTH}x{WIN_HEIGHT}")
+
+
+
+    window.mainloop()
 
 if __name__ == "__main__":
     main()
