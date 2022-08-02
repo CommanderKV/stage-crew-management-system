dc48a9cfdb3a6cd0d753a6f288917c39b6c7885f
kyler <kylervisser@gmail.com>
2022-08-02 01:05:20 -0400

    modified:   main.py

--- main.py
+++ main.py
@@ -58,18 +58,19 @@ db format:
 
 
 import os
+from re import L
 import tkinter
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 from datetime import date as date_year
-import json
+from datetime import datetime
+from tkcalendar import Calendar
 
 # Initialize Firebase
 cred = credentials.Certificate(os.path.join(os.getcwd(), "stage-crew-event-manager-firebase-adminsdk-ed2pg-b047e32b38.json"))
 DB_CONNECTION = firebase_admin.initialize_app(cred, {"databaseURL": "https://stage-crew-event-manager-default-rtdb.firebaseio.com"})
 DB_REFERENCE = db.reference("/")
-
 
 
 
@@ -93,7 +94,8 @@ def save_student_info(fname: str, lname: str, email: str, events: str, ref: db.r
         "first name": fname,
         "last name": lname,
         "email": email,
-        "events": int (events)
+        "events": int (events),
+        "version": 1.1
     }
 
     try:
@@ -111,14 +113,24 @@ def save_student_info(fname: str, lname: str, email: str, events: str, ref: db.r
             ref.push().set(data)
             return True
 
+        version_updater(ref)
+
     except Exception as e:
         print(e)
         return False
 
 
-def get_students(ref) -> list or None:
-    if ref.get() is None:
+def get_students() -> list or None:
+    ref = db.reference("/")
+    ref_data = ref.get()
+
+    if ref_data is None:
         ref.set({"Students": {}})
+    
+    elif "Students" not in ref_data.keys():
+        ref_data["Students"] = {}
+        ref.set(ref_data)
+
     ref = db.reference("/Students")
     
     students = []
@@ -132,11 +144,12 @@ def get_students(ref) -> list or None:
         return students
 
 
-def save_event_info(name: str, date: str, start_time: str, end_time: str, sound: bool, mics: bool, lights: bool, projector: bool, ref: db.reference, update_events: bool=False) -> bool:
+def save_event_info(name: str, date: str, start_time: str, end_time: str, sound: bool, mics: bool, lights: bool, projector: bool, teacher_email: str) -> bool:
     # -------------------------
     #    Check if directory 
     #   exists if not make it
     # -------------------------
+    ref = db.reference("/")
     ref_data = ref.get()
 
     if ref_data is None:
@@ -160,32 +173,106 @@ def save_event_info(name: str, date: str, start_time: str, end_time: str, sound:
         "sound": sound,
         "mics": mics,
         "lights": lights,
-        "projector": projector
+        "projector": projector,
+        "teacher contact": teacher_email,
+        "version": 1.1
     }
 
     # -------------------------
     #   Add event to database
     # -------------------------
-    #try:
-    if True:
+    try:
         items = ref.get()
         if items is not None:
-            print (items)
             for item in items:
                 if items[item]["name"] == name:
-                    if update_events:
-                        ref.child(item).update(data)
-                        return True
-
-                    else:
                         return False
+
+                else:
+                    ref.push().set(data)
+                    return True
         else:
             ref.push().set(data)
             return True
+        
+        version_updater(ref)
 
-    # except Exception as e:
-    #     print(e)
-    #     return False
+    except Exception as e:
+        print(e)
+        return False
+
+
+def get_all_events():
+    ref = db.reference("/")
+    ref_data = ref.get()
+
+    if ref_data is None:
+        ref.set({"Events": {}})
+
+    elif "Events" not in ref_data.keys():
+        ref_data["Events"] = {}
+        ref.set(ref_data)
+    
+    ref = db.reference("/Events")
+    
+    data = ref_data["Events"]
+    results = []
+    for key, value in data.items():
+        value["key"] = key
+        results.append(value)
+    
+    return results
+
+
+def version_updater(ref: db.reference):
+    section = ref.path
+    ref_data = ref.get()
+    past_ref_value = db.reference(section).get()
+
+    temp = db.reference("/Version").get()
+
+    if "/" == section or section is None:
+        print("No data")
+        return False
+    
+    elif "Students" in section:
+        current_version = int(temp["Students"]["v"])
+
+    elif "Events" in section:
+        current_version = int(temp["Events"]["v"])
+
+    updated = False
+
+    # each dict
+    for key, value in ref_data.items():
+        # each student/event in dict
+        if "version" not in value.keys():
+            value["version"] = 0
+
+        if value["version"] < current_version:
+            template = db.reference("/Version"+section+"/template").get()
+
+            # Find missing keys and add them
+            if template.keys() != value.keys():
+                for key2 in template.keys():
+                    if key2 not in value.keys():
+                        ref_data[key][key2] = template[key2]
+
+            value["version"] = current_version
+            updated = True
+
+    if updated:
+        # Add everything to the database
+        ref = db.reference(section)
+        ref.transaction(lambda value: value if past_ref_value != value else ref_data)
+
+        ref = db.reference("/Version" + section + "/v")
+        ref.transaction(lambda value: value + .1 if value < current_version+.1 else value)
+        if ref.get() > current_version+.1:
+            version_updater(db.reference(section))
+        else:
+            return True
+
 
 
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\
@@ -211,6 +298,7 @@ def check_time(time: str) -> bool:
 
     else:
         return False
+
 
 def check_date(date: str) -> bool:
     numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
@@ -375,7 +463,7 @@ def submit_event(name: str, date: str, time: str, end_time: str, teacher_contact
                 if end_time != "" and end_time != "hh:mm" and end_time.replace(" ", "") != "" and len(end_time) == 5 and check_time(end_time):
                     if teacher_contact != "" and teacher_contact.replace(" ", "") != "" and "tdsb.on.ca" in teacher_contact and "@" in teacher_contact and len(teacher_contact) >= 20:
                         crate_event_error_message.configure(text="")
-                        if not save_event_info(name, date, time, end_time, sound, mics, lights, projector, ref):
+                        if not save_event_info(name, date, time, end_time, sound, mics, lights, projector, teacher_contact, ref):
                             crate_event_error_message.configure(text="Error uploading event or event already exists.")
                             return False
 
@@ -630,14 +718,434 @@ def create_event():
     create_event_window.mainloop()
 
 
+def view_events():
+    global window, view_events_window, EVENTS
+    window.destroy()
+
+    # ------------------------------------
+    #   Get all events from the calendar
+    # ------------------------------------
+    received_events = get_all_events()
+
+
+    def update_displayed_events(cal: Calendar):
+        # ------------------------------------------
+        #   Add all received events to the calendar
+        # ------------------------------------------
+        for event in received_events:
+
+            displayed_month, displayed_year = cal.get_displayed_month_year()
+            if int(event["date"].split("-")[1]) == displayed_month and int(event["date"].split("-")[0]) == displayed_year:
+                time = datetime.strptime(event["date"], "%Y-%m-%d")
+                details = f'{event["name"]}|{event["date"]}|{event["start time"]}|{event["end time"]}|{event["mics"]}|{event["lights"]}|{event["projector"]}|{event["sound"]}|{event["teacher contact"]}|{event["key"]}'
+                cal.calevent_create(time, details, "Message")
+        
+        cal.tag_config("Message", background="blue", foreground="black")
+
+
+    class CustomCalendar(Calendar):
+        def _next_month(self):
+            Calendar._next_month(self)
+            self.event_generate("<<CalendarMonthChanged>>")
+            update_displayed_events(self)
+
+
+        def _prev_month(self):
+            Calendar._prev_month(self)
+            self.event_generate("<<CalendarMonthChanged>>")
+            update_displayed_events(self)
+
+
+        def _next_year(self):
+            Calendar._next_year(self)
+            self.event_generate("<<CalendarMonthChanged>>")
+            update_displayed_events(self)
+
+
+        def _prev_year(self):
+            Calendar._prev_year(self)
+            self.event_generate("<<CalendarMonthChanged>>")
+            update_displayed_events(self)
+
+
+        def get_displayed_month_year(self):
+            return self._date.month, self._date.year
+
+
+
+    view_events_window = tkinter.Tk()
+    EVENTS = []
+
+    def cal_day_selected(_=None):
+        global EVENTS
+        if cal.get_calevents(cal.selection_get()):
+            events = cal.get_calevents(cal.selection_get())
+            if EVENTS != events:
+                EVENTS = [[event, 0] for event in events]
+                EVENTS[0][1] = 1
+                display_event(EVENTS[0][0])
+            
+
+    def display_event(event_id):
+        text = cal.calevent_cget(event_id, "text")
+        text = text.split("|")
+
+        # -------------------------------------------
+        #   Set up all labels according to the text
+        # -------------------------------------------
+        entrys = [
+            (event_name_label, text[0]),
+            (date_label, text[1]),
+            (start_time_label, text[2]),
+            (end_time_label, text[3]),
+            (teacher_contact_label, text[8])
+        ]
+
+        for entry, text in entrys:
+            entry.configure(state="normal")
+            entry.insert(0, text)
+            entry.configure(state="disabled")
+
+
+        checks = [
+            (mics_checkbutton_value, text[4]),
+            (lights_checkbutton_value, text[5]),
+            (projector_checkbutton_value, text[6]),
+            (sound_checkbutton_value, text[7])
+        ]
+
+        for check, text in checks:
+            if text == "True":
+                check.set(1)
+
+            else:
+                check.set(0)
+
+
+    def next_event():
+        if len(EVENTS) > 0:
+            starting_pos = None
+            for pos, event in enumerate(EVENTS):
+                if event[1] == 1:
+                    starting_pos = pos
+                    break
+
+            EVENTS[starting_pos][1] = 0
+            
+            if starting_pos != None:
+                pos = starting_pos
+                while pos < len(EVENTS):
+                    pos += 1
+                    if EVENTS[pos][1] == 0:
+                        display_event(EVENTS[pos][0])
+                        break
+
+
+    def back_event():
+        if len(EVENTS) > 0:
+            starting_pos = None
+            for pos, event in enumerate(EVENTS.reverse()):
+                if event[1] == 1:
+                    starting_pos = pos
+                    break
+            
+            EVENTS[starting_pos][1] = 0
+
+            if starting_pos != None:
+                pos = starting_pos
+                while pos > len(EVENTS.reverse()):
+                    pos -= 1
+                    if EVENTS[pos][1] == 0:
+                        display_event(EVENTS[pos][0])
+                        break
+
+
+    def save():
+        def update(value):
+            data = {
+                "name": 0,
+                "date": 1,
+                "start time": 2,
+                "end time": 3,
+                "mics": 4,
+                "lights": 5,
+                "projector": 6,
+                "sound": 7,
+                "teacher contact": 8
+            }
+            for key, value2 in data.items():
+                value[key] = text[value2]
+            
+            return value
+
+        if len(EVENTS) > 0:
+            event_id = None
+            for event in EVENTS:
+                if event[1] == 1:
+                    event_id = event[0]
+                    break
+            
+            text = cal.calevent_cget(event_id, "text").split("|")
+            key = text[-1]
+
+            db.reference("/Events/"+key).transaction(update)
+
+
+
+    # -------------------
+    #   Custom calendar
+    # -------------------
+    cal = CustomCalendar(view_events_window, selectmode="day")
+    cal.bind("<<CalendarSelected>>", cal_day_selected)
+    cal_day_selected()
+    update_displayed_events(cal)
+
+
+    # -----------------------
+    #   Event details frame
+    # -----------------------
+    event_details_frame = tkinter.Frame(view_events_window)
+
+
+    # -----------------------
+    #   Event details label
+    # -----------------------
+    event_details = tkinter.Label(
+        event_details_frame, 
+        text="Event details",
+        font=("Comic Sans MS", 18, "bold")
+    )
+    event_details.grid(row=0, column=0, columnspan=2)
+
+
+    # --------------------
+    #   Event name label
+    # --------------------
+    event_name = tkinter.Label(event_details_frame, text="Event name:")
+    event_name.grid(row=1, column=0)
+
+
+    # --------------------------
+    #   Event name entry label
+    # --------------------------
+    event_name_label = tkinter.Entry(event_details_frame)
+    event_name_label.config(state=tkinter.DISABLED)
+    event_name_label.grid(row=1, column=1)
+
+
+    # --------
+    #   Date
+    # --------
+    date = tkinter.Label(event_details_frame, text="Date:")
+    date.grid(row=2, column=0)
+
+
+    # --------------------
+    #   Date entry label
+    # --------------------
+    date_label = tkinter.Entry(event_details_frame)
+    date_label.config(state=tkinter.DISABLED)
+    date_label.grid(row=2, column=1)
+
+
+    # --------------
+    #   Start time
+    # --------------
+    start_time = tkinter.Label(event_details_frame, text="Start time:")
+    start_time.grid(row=3, column=0)
+
+
+    # --------------------------
+    #   Start time entry label
+    # --------------------------
+    start_time_label = tkinter.Entry(event_details_frame)
+    start_time_label.config(state=tkinter.DISABLED)
+    start_time_label.grid(row=3, column=1)
+
+
+    # ------------
+    #   End time
+    # ------------
+    end_time = tkinter.Label(event_details_frame, text="End time:")
+    end_time.grid(row=4, column=0)
+
+
+    # ------------------------
+    #   End time entry label
+    # ------------------------
+    end_time_label = tkinter.Entry(event_details_frame)
+    end_time_label.config(state=tkinter.DISABLED)
+    end_time_label.grid(row=4, column=1)
+
+
+    # -------------------
+    #   Teacher contact
+    # -------------------
+    teacher_contact = tkinter.Label(event_details_frame, text="Teacher contact:")
+    teacher_contact.grid(row=5, column=0)
+
+
+    # -------------------------------
+    #   Teacher contact entry label
+    # -------------------------------
+    teacher_contact_label = tkinter.Entry(event_details_frame)
+    teacher_contact_label.config(state=tkinter.DISABLED)
+    teacher_contact_label.grid(row=5, column=1)
+
+
+    # ---------
+    #   Sound
+    # ---------
+    sound = tkinter.Label(event_details_frame, text="Sound:")
+    sound.grid(row=6, column=0)
+
+
+    # ---------------------------
+    #   Sound entry checkbutton
+    # ---------------------------
+    sound_checkbutton_value = tkinter.IntVar()
+    sound_checkbutton = tkinter.Checkbutton(
+        event_details_frame, 
+        state="disabled", 
+        variable=sound_checkbutton_value, 
+        onvalue=1, 
+        offvalue=0
+    )
+    sound_checkbutton.grid(row=6, column=1)
+
+
+    # --------
+    #   Mics 
+    # --------
+    mics = tkinter.Label(event_details_frame, text="Mics:")
+    mics.grid(row=7, column=0)
+
+
+    # --------------------------
+    #   Mics entry checkbutton
+    # --------------------------
+    mics_checkbutton_value = tkinter.IntVar()
+    mics_checkbutton = tkinter.Checkbutton(
+        event_details_frame,
+        state="disabled",
+        variable=mics_checkbutton_value,
+        onvalue=1,
+        offvalue=0
+    )
+    mics_checkbutton.grid(row=7, column=1)
+
+
+    # ----------
+    #   Lights
+    # ----------
+    lights = tkinter.Label(event_details_frame, text="Lights:")
+    lights.grid(row=8, column=0)
+
+
+    # ----------------------------
+    #   Lights entry checkbutton
+    # ----------------------------
+    lights_checkbutton_value = tkinter.IntVar()
+    lights_checkbutton = tkinter.Checkbutton(
+        event_details_frame,
+        state="disabled",
+        variable=lights_checkbutton_value,
+        onvalue=1,
+        offvalue=0
+    )
+    lights_checkbutton.grid(row=8, column=1)
+
+
+    # -------------
+    #   Projector
+    # -------------
+    projector = tkinter.Label(event_details_frame, text="Projector:")
+    projector.grid(row=9, column=0)
+
+
+    # -------------------------------
+    #   Projector entry checkbutton
+    # -------------------------------
+    projector_checkbutton_value = tkinter.IntVar()
+    projector_checkbutton = tkinter.Checkbutton(
+        event_details_frame,
+        state="disabled",
+        variable=projector_checkbutton_value,
+        onvalue=1,
+        offvalue=0
+    )
+    projector_checkbutton.grid(row=9, column=1)
+
+
+    # ---------------
+    #   Next button
+    # ---------------
+    next_button = tkinter.Button(
+        event_details_frame,
+        text="Next Event",
+        command=next_event
+    )
+    next_button.grid(row=10, column=1)
+
+
+    # ----------------------
+    #   Back button events
+    # ----------------------
+    back_button = tkinter.Button(
+        event_details_frame,
+        text="Previous event",
+        command=back_event
+    )
+    back_button.grid(row=10, column=0)
+
+
+    # ---------------
+    #   Save button
+    # ---------------
+    save_button = tkinter
+
+
+    # -----------------------
+    #   Error message label
+    # -----------------------
+    error_message = tkinter.Label(view_events_window, text="")
+    error_message.grid(row=1, column=0, columnspan=2)
+
+
+    # ---------------
+    #   Back button
+    # ---------------
+    back_button_main = tkinter.Button(
+        view_events_window,
+        text="Back",
+        command=lambda: main()
+    )
+    back_button_main.grid(row=5, column=0, columnspan=3)
+
+
+    # -------------------------------
+    #   view_events_window mainloop
+    # -------------------------------
+    cal.grid(row=0, column=0, columnspan=2)
+    event_details_frame.grid(row=0, column=3)
+    view_events_window.mainloop()
+
+
+
 
 def main():
-    global window, WIN_WIDTH, WIN_HEIGHT, add_student_window, create_event_window
+    global window, WIN_WIDTH, WIN_HEIGHT, add_student_window, create_event_window, view_events_window
     if add_student_window != None:
         add_student_window.destroy()
+        add_student_window = None
 
     if create_event_window != None:
         create_event_window.destroy()
+        create_event_window = None
+    
+    if view_events_window != None:
+        view_events_window.destroy()
+        view_events_window = None
 
 
     window = tkinter.Tk()
@@ -686,6 +1194,17 @@ def main():
     create_event_button.grid(row=0, column=1)
 
 
+    # ----------------------
+    #   View events button
+    # ----------------------
+    view_events_button = tkinter.Button(
+        button_frame,
+        text="View events",
+        command=view_events,
+    )
+    view_events_button.grid(row=0, column=2)
+
+
     # -------------------
     #   Start main loop
     # -------------------
@@ -698,4 +1217,5 @@ if __name__ == "__main__":
 
     create_event_window = None
     add_student_window = None
+    view_events_window = None
     main()
